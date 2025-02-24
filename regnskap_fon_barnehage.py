d5e088cbbc9a45f6a167057aec5838cfc2ea8cb8
Even Lislebø <even.lislebo@skagerakenergi.no>
2025-02-24 14:10:54 +0100

    calculate average per year

--- regnskap_fon_barnehage.py
+++ regnskap_fon_barnehage.py
@@ -13,20 +13,30 @@ def read_csv(filename: str) -> pd.DataFrame:
 if __name__ == "__main__":
     codes_df = read_csv("utgiftskonti.csv")
     accounts_df = read_csv("regnskap_fon_barnehage.csv")
-    by_year = accounts_df.groupby("År")
-    for y in by_year:
-        print(y)
-    relevant_codes = codes_df.loc[codes_df["Driftskonto?"] == "y"]
+    codes = accounts_df[["KOSTRA-kode", "Beskrivelse"]].drop_duplicates(subset="KOSTRA-kode")
+    codes["Beskrivelse"] = codes["Beskrivelse"].map(str.capitalize)
+    print(f"{len(codes)} unique codes:")
+    print(codes)
+    with open("kostra_koder_i_regnskap.csv", "w+") as codes_file:
+        codes.to_csv(codes_file, index=False, sep=";")
+    relevant_codes = codes_df.loc[codes_df["Driftsutgift?"] == "y"]
+    print(f"{len(relevant_codes)} relevante utgiftskonti:")
     print(relevant_codes)
 
-    driftsutgifter_df = accounts_df.loc[accounts_df["KOSTRA-kode"].isin(relevant_codes["Kode"].tolist())]
+    driftsutgifter_df = accounts_df.loc[accounts_df["KOSTRA-kode"].isin(relevant_codes["KOSTRA-kode"].tolist())]
     print(driftsutgifter_df)
 
     driftsutgifter_df["Verdi"] = driftsutgifter_df["Verdi"].str.replace(",", "").astype(int)
+    tot = 0
     for year in driftsutgifter_df["År"].unique():
         print(f"{year}:")
         for entry in driftsutgifter_df.loc[driftsutgifter_df["År"] == year].itertuples():
             print(entry)
 
         sum = driftsutgifter_df.loc[driftsutgifter_df["År"] == year]["Verdi"].sum()
-        print(sum)
+
+        tot += sum
+        print(f"sum: {sum}")
+    print(f"total: {tot}")
+    number_of_years = len(driftsutgifter_df["År"].unique())
+    print(f"gjennomsnitt ({number_of_years} år): {round(tot / number_of_years, ndigits=1)}")
